d6d88ac57d2f9cdb100bc0ed2f13eeb6ea7f4c09
Evan Pavley <epavley@sva.edu>
2018-12-06 14:33:57 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,6 +15,8 @@ def hopper
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:grace_hopper]
 
 
 end
@@ -37,6 +39,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +58,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages].join()
 end
 
 def adding_matz
@@ -79,8 +83,9 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
+     
+     programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+     programmer_hash
 end
 
 def changing_alan
@@ -103,6 +108,9 @@ def changing_alan
      alans_new_info = "GUI"
      
      
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     programmer_hash
+     
 end
 
 def adding_to_dennis
@@ -121,5 +129,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+     programmer_hash
 
 end
